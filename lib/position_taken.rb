a93c2c652ce269beeb4f559c4432e9f116d3c897
dieter long <dieterwaitforitlong@gmail.com>
2021-04-16 20:41:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,5 +4,9 @@ def position_taken?(board, index)
      false
      elsif board[index] == " "
      false
+     elsif board[index] == "X" or "O"
+     true 
+     elsif board[index] == nil 
+     false
 end
   end
